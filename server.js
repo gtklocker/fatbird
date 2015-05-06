a9617ada9a4ef230f1101648e2209303ffd6d828
Kostis Karantias <karantiaskostis@gmail.com>
2015-05-06 16:29:29 +0300

    Account for the API being outdated, closes #1

--- server.js
+++ server.js
@@ -9,10 +9,33 @@ var TIMEZONE = "Europe/Athens";
 
 var schedule;
 
-function noteFactory(title, attr) {
+function getMeal(time) {
+    var dateString = (new Date()).toISOString().replace(/T.+/, "");
+    var realDay;
+
+    if (schedule["shmera"]["day"] == dateString) {
+        realDay = "shmera";
+    }
+    else if (schedule["ayrio"]["day"] == dateString) {
+        realDay = "ayrio";
+    }
+
+    if (!realDay) {
+        // API is seriously outdated!
+        return undefined;
+    }
+
+    return schedule[realDay][time]["kyriosPiata"];
+}
+
+function noteFactory(title, time) {
     return function() {
+        if (!getMeal(time)) {
+            console.log("no food schedule for today, can't push out notification");
+            return;
+        }
         console.log("pushing notification for", title);
-        pusher.note("", title, schedule["shmera"][attr]["kyriosPiata"], function(err, res) {
+        pusher.note("", title, getMeal(time), function(err, res) {
             if (err) {
                 console.log(err);
                 return;
